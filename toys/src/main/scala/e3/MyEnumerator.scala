b5e5a7ab88897675e202b2c4da3f57b67413c419
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2013-01-03 23:13:42 +0200

    splitted into foew files.

---
+++ toys/src/main/scala/e3/MyEnumerator.scala
@@ -0,0 +1,48 @@
+package e3
+
+trait MyEnumerator[A]
+{
+
+  self =>
+
+  def apply[S](it: MyIteratee[A,S]): MyIteratee[A,S]
+
+  def andThen(x: MyEnumerator[A]) = new MyEnumerator[A] {
+       def apply[S](it: MyIteratee[A,S]) = 
+             x(self(it))
+  }
+
+  def eof() = andThen(new PutEOF[A])
+
+}
+
+case class PutOne[A](a:A) extends MyEnumerator[A]
+{
+
+   def apply[S](it: MyIteratee[A,S]):MyIteratee[A,S] =
+          it.fold( k => k(Input.El(a)), Function.const(it) )
+                 
+}
+
+
+class PutEOF[A] extends MyEnumerator[A]
+{
+
+   def apply[S](it: MyIteratee[A,S]):MyIteratee[A,S] =
+          it.fold( k => k(Input.EOF), Function.const(it) )
+
+}
+
+
+case class ListEnumerator[A](l:List[A]) extends MyEnumerator[A]
+{
+ 
+   def apply[S](it: MyIteratee[A,S]):MyIteratee[A,S] =
+     l.foldLeft(it)((it,e) => 
+          it.fold( step => step(Input.El(e)), Function.const(it) ))
+
+
+}
+
+
+
